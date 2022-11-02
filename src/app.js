7a08e0ed4f8ad1aeac25a081b596dd98321f29a9
Yang-ah <yangah0409@gmail.com>
2022-11-02 21:17:22 +0900

    fifth commit : made data

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// 1. make data objects
+const data = {
+  USD: {
+    USD: 1,
+    KRW: 1414.66,
+    JPY: 147.11,
+    CNY: 7.28,
+    unit: "dallars",
+  },
+  KRW: {
+    KRW: 1,
+    USD: 0.00071,
+    JPY: 0.1,
+    CNY: 0.0051,
+    unit: "won",
+  },
+  JPY: {
+    JPY: 1,
+    KRW: 9.62,
+    USD: 0.0068,
+    CNY: 0.05,
+    unit: "yen",
+  },
+
+  CNY: {
+    CNY: 1,
+    JPY: 20.2,
+    KRW: 194.3,
+    USD: 0.14,
+    unit: "yuan",
+  },
+};
+
+// 2. select currency
+
+// 2-1. change content of btn
+// 2-2. change txt
+
+// 3. convert
+// as value of each btns, change each output
+
+// main
